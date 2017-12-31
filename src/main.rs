15c6a453296cede8269ec17e7ea5cc01663e39dd
Martin Persson <mnpn03@gmail.com>
2017-12-31 19:59:17 +0100

    Initial GTK stuff! :^)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,44 @@
 extern crate aqrpm;
+extern crate gtk;
+
+use gtk::{
+    Align,
+    Box as GtkBox,
+    Button,
+    ButtonsType,
+    ContainerExt,
+    GtkWindowExt,
+    Label,
+    Menu,
+    MenuItem,
+    MessageDialog,
+    RadioButton,
+    WidgetExt,
+    Window,
+    WindowType
+};
 
 fn main() {
+	if let Err(err) = gtk::init() {
+		eprintln!("gtk error: {}", err);
+		return;
+	}
+
     println!("Hello, world!");
+
+	let window = Window::new(WindowType::Toplevel);
+	window.set_title("AQRPM - Vault");
+	window.set_default_size(1280, 720);
+    window.set_border_width(10);
+    window.set_position(gtk::WindowPosition::Center);
+
+    let button = gtk::Button::new_with_label("i am a giant temporary button, bitch!");
+    //let label = gtk::Label::new("watch out for that button!");
+
+    window.add(&button);
+    //window.add(&label);
+
+	window.show_all();
+
+	gtk::main();
 }
